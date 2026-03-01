2a8b9806182e82cd7064d2d9878b492adf615f65
Lakshay Chauhan <lakshaychauhan129@gmail.com>
2026-03-01 17:12:15 +0530

    change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,24 @@ import { CommonModule } from '@angular/common';
 import { DataLabelContainer } from './component/data-label-container/data-label-container';
 import { MatIconModule } from '@angular/material/icon';
 import { Sidebar } from './component/sidebar/sidebar';
+import { FormsModule } from '@angular/forms';
+import { Mainboard } from './component/mainboard/mainboard';
 
 @NgModule({
   declarations: [
     DataLabelContainer,
-    Sidebar
+    Sidebar,
+    Mainboard
   ],
   imports: [
     CommonModule,
     MatIconModule,
+    FormsModule
   ],
   exports:[
     DataLabelContainer,
-    Sidebar
+    Sidebar,
+    Mainboard
   ]
 })
 export class AppModule { }
